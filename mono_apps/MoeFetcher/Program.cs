13e160b72e0b74c336665c8b4896643f38dabbc8
filip <bitbucket@xn--feld-4qa.de>
2017-02-15 23:26:37 +0100

    fiddling with WG API

---
+++ mono_apps/MoeFetcher/Program.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoeFetcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object foo;
+            string testJson = @"""data"": {""052124"": [{}] }";
+            using (Stream stream = File.OpenRead(@"F:\repos\3-moe-website\mono_apps\MoeFetcher\MoeList.json"))
+            using (StreamReader reader = new StreamReader(stream))
+                foo = JsonConvert.DeserializeObject<Foo>(reader.ReadToEnd());
+            //var foo = JsonConvert.DeserializeObject<Foo>(testJson);
+        }
+    }
+
+    class Foo
+    {
+        public DataWrapper data { get; set; }
+    }
+
+    class DataWrapper
+    {
+        [JsonProperty(Order = 1)]
+        public Tank[] foo { get; set; }
+    }
+
+    class Tank
+    {
+        public MarkObject achievements { get; set; }
+        public long account_id { get; set; }
+        public int tank_id { get; set; }
+    }
+
+    class MarkObject
+    {
+        public int markOfMastery { get; set; }
+    }
+}
