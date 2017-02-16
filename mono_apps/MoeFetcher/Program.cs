9f323858b4591f66a54aa896f3928b77d9677ae5
filip <bitbucket@xn--feld-4qa.de>
2017-02-16 23:49:42 +0100

    more fiddling

--- mono_apps/MoeFetcher/Program.cs
+++ mono_apps/MoeFetcher/Program.cs
@@ -15,9 +15,17 @@ namespace MoeFetcher
             object foo;
             string testJson = @"""data"": {""052124"": [{}] }";
             using (Stream stream = File.OpenRead(@"F:\repos\3-moe-website\mono_apps\MoeFetcher\MoeList.json"))
-            using (StreamReader reader = new StreamReader(stream))
-                foo = JsonConvert.DeserializeObject<Foo>(reader.ReadToEnd());
+            using (StreamReader streamReader = new StreamReader(stream))
+            //foo = JsonConvert.DeserializeObject<Foo>(reader.ReadToEnd());
             //var foo = JsonConvert.DeserializeObject<Foo>(testJson);
+
+            using (JsonTextReader reader = new JsonTextReader(streamReader))
+            {
+                while (reader.Read())
+                    if (reader.TokenType == JsonToken.PropertyName)
+                    Console.WriteLine(reader.Value);
+            }
+            Console.Read();
         }
     }
 
