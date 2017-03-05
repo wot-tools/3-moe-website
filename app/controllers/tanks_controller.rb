0fd0195827e6cd13474829df0738f262d9c4a4c7
Aimdrol <accounts_aimdrol@hotmail.com>
2017-03-05 18:54:36 +0100

    Eager loading tanks

--- app/controllers/tanks_controller.rb
+++ app/controllers/tanks_controller.rb
@@ -2,18 +2,18 @@ class TanksController < ApplicationController
 
 	def index
 		@q = Tank.ransack(params[:q])
-		@tanks = @q.result.includes(:marks, :nation, :vehicle_type).paginate(:page => params[:page], :per_page => 20)
+		@tanks = @q.result.includes(:nation, :vehicle_type).paginate(:page => params[:page], :per_page => 20)
 	end
 	
 	def index_winrate
 		@q = Tank.ransack(params[:q])
-		@tanks = @q.result.includes(:marks, :nation, :vehicle_type).paginate(:page => params[:page], :per_page => 20)
+		@tanks = @q.result.includes(:nation, :vehicle_type).paginate(:page => params[:page], :per_page => 20)
 		@tanks_calculation = @q.result.joins("INNER JOIN marks ON tanks.id=marks.tank_id").joins("INNER JOIN players ON marks.player_id=players.id");
 	end
 	
 	def index_wn8
 		@q = Tank.ransack(params[:q])
-		@tanks = @q.result.includes(:marks, :nation, :vehicle_type).paginate(:page => params[:page], :per_page => 20)
+		@tanks = @q.result.includes(:nation, :vehicle_type).paginate(:page => params[:page], :per_page => 20)
 		@tanks_calculation = @q.result.joins("INNER JOIN marks ON tanks.id=marks.tank_id").joins("INNER JOIN players ON marks.player_id=players.id");
 	end
 	
@@ -21,7 +21,7 @@ class TanksController < ApplicationController
 		@tank = Tank.find(params[:id])
 		
 		@q = @tank.marks.ransack(params[:q])
-		@marks = @q.result.distinct.paginate(:page => params[:page], :per_page => 20)
+		@marks = @q.result.distinct.includes(:player, player: [:clan]).paginate(:page => params[:page], :per_page => 20)
 		
 		rescue ActiveRecord::RecordNotFound
 			redirect_to tanks_path and return
