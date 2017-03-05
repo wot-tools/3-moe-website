30c2a419f971655987eed32d20ab86f525d18863
Aimdrol <accounts_aimdrol@hotmail.com>
2017-03-05 18:55:02 +0100

    Added eager loading to marks

--- app/controllers/marks_controller.rb
+++ app/controllers/marks_controller.rb
@@ -1,9 +1,8 @@
 class MarksController < ApplicationController
 
 	def index
-		#@marks = Mark.all
 		@q = Mark.all.ransack(params[:q])
-		@marks = @q.result.paginate(:page => params[:page], :per_page => 20)
+		@marks = @q.result.includes(:player, player: [:clan]).paginate(:page => params[:page], :per_page => 20)
 	end
 
 end
