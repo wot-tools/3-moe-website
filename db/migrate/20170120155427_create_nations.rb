374dfb002c9acead62f0d9ec70c43f502422ec18
Aimdrol <accounts_aimdrol@hotmail.com>
2017-03-07 19:26:24 +0100

    Moved moe_rating and mark_count column creation to table creation

--- db/migrate/20170120155427_create_nations.rb
+++ db/migrate/20170120155427_create_nations.rb
@@ -1,7 +1,8 @@
 class CreateNations < ActiveRecord::Migration[5.0]
   def change
     create_table :nations do |t|
-      t.string :name
+      t.string :name, null: false
+	  t.integer :mark_count, null: false, default: 0
 
       t.timestamps
     end
--- db/migrate/20170121214444_create_clans.rb
+++ db/migrate/20170121214444_create_clans.rb
@@ -1,10 +1,12 @@
 class CreateClans < ActiveRecord::Migration[5.0]
   def change
     create_table :clans do |t|
-      t.string :name
-      t.string :tag
-      t.string :cHex
-      t.integer :members
+      t.string :name, null: false
+      t.string :tag, null: false
+      t.string :cHex, null: false, default: "#FFFFFF"
+      t.integer :members, null: false, default: 0
+	  t.integer :mark_count, null: false, default: 0
+	  t.integer :moe_rating, null: false, default: 0
       t.datetime :updatedAtWG
 	  t.datetime :clanCreated
       t.string :icon24px
