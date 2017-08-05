cfd5c8f193674bdfeb5332a1b6083323f8fccfd2
Marvin Hopp <accounts_aimdrol@hotmail.com>
2017-08-05 17:33:16 +0200

    Updated db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,96 +10,99 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170216230446) do
-
-  create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "title"
-    t.text     "text",       limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-  end
+ActiveRecord::Schema.define(version: 20170122000148) do
 
   create_table "clans", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name"
-    t.string   "tag"
-    t.string   "cHex"
-    t.integer  "members"
-    t.datetime "updatedAtWG"
-    t.datetime "clanCreated"
-    t.string   "icon24px"
-    t.string   "icon32px"
-    t.string   "icon64px"
-    t.string   "icon195px"
-    t.string   "icon256px"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.integer  "moe_rating"
-    t.integer  "mark_count",  default: 0, null: false
+    t.string   "name",                            null: false
+    t.string   "tag",                             null: false
+    t.string   "cHex",        default: "#FFFFFF", null: false
+    t.integer  "members",     default: 0,         null: false
+    t.integer  "mark_count",  default: 0,         null: false
+    t.integer  "moe_rating",  default: 0,         null: false
+    t.datetime "updatedAtWG",                     null: false
+    t.datetime "clanCreated",                     null: false
+    t.string   "icon24px",                        null: false
+    t.string   "icon32px",                        null: false
+    t.string   "icon64px",                        null: false
+    t.string   "icon195px",                       null: false
+    t.string   "icon256px",                       null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
   end
 
   create_table "marks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "tank_id"
-    t.integer  "player_id"
+    t.integer  "tank_id",    null: false
+    t.integer  "player_id",  null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["player_id"], name: "index_marks_on_player_id", using: :btree
+    t.index ["tank_id", "player_id"], name: "index_marks_on_tank_id_and_player_id", unique: true, using: :btree
     t.index ["tank_id"], name: "index_marks_on_tank_id", using: :btree
   end
 
   create_table "nations", id: :string, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name"
+    t.string   "name",                   null: false
+    t.integer  "mark_count", default: 0, null: false
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
-    t.integer  "mark_count", default: 0, null: false
   end
 
   create_table "players", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name"
-    t.integer  "battles"
-    t.integer  "wgrating"
-    t.float    "winratio",       limit: 24
-    t.datetime "lastLogout"
-    t.datetime "lastBattle"
-    t.datetime "accountCreated"
-    t.datetime "updatedAtWG"
-    t.integer  "wn8"
-    t.string   "clientLang"
+    t.string   "name",                                    null: false
+    t.integer  "battles",                   default: 0,   null: false
+    t.integer  "wn8",                       default: 0,   null: false
+    t.integer  "wgrating",                  default: 0,   null: false
+    t.float    "winratio",       limit: 24, default: 0.0, null: false
+    t.integer  "mark_count",                default: 0,   null: false
+    t.integer  "moe_rating",                default: 0,   null: false
+    t.datetime "lastLogout",                              null: false
+    t.datetime "lastBattle",                              null: false
+    t.datetime "accountCreated",                          null: false
+    t.datetime "updatedAtWG",                             null: false
+    t.string   "clientLang",                              null: false
     t.integer  "clan_id"
-    t.datetime "created_at",                            null: false
-    t.datetime "updated_at",                            null: false
-    t.integer  "moe_rating"
-    t.integer  "mark_count",                default: 0, null: false
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
     t.index ["clan_id"], name: "index_players_on_clan_id", using: :btree
   end
 
   create_table "tanks", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.boolean  "ispremium"
-    t.string   "name"
-    t.string   "shortname"
-    t.integer  "tier"
-    t.string   "bigicon"
-    t.string   "contouricon"
-    t.string   "smallicon"
-    t.string   "nation_id"
-    t.string   "vehicle_type_id"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.integer  "moe_value"
-    t.integer  "mark_count",      default: 0, null: false
+    t.boolean  "ispremium",       default: false, null: false
+    t.string   "name",                            null: false
+    t.string   "shortname",                       null: false
+    t.integer  "mark_count",      default: 0,     null: false
+    t.integer  "moe_value",       default: 0,     null: false
+    t.string   "bigicon",                         null: false
+    t.string   "contouricon",                     null: false
+    t.string   "smallicon",                       null: false
+    t.integer  "tier_id",                         null: false
+    t.string   "nation_id",                       null: false
+    t.string   "vehicle_type_id",                 null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.index ["nation_id"], name: "index_tanks_on_nation_id", using: :btree
+    t.index ["tier_id"], name: "index_tanks_on_tier_id", using: :btree
     t.index ["vehicle_type_id"], name: "index_tanks_on_vehicle_type_id", using: :btree
   end
 
-  create_table "vehicle_types", id: :string, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name"
+  create_table "tiers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",                   null: false
+    t.integer  "mark_count", default: 0, null: false
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+  end
+
+  create_table "vehicle_types", id: :string, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",                   null: false
     t.integer  "mark_count", default: 0, null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   add_foreign_key "marks", "players"
   add_foreign_key "marks", "tanks"
   add_foreign_key "players", "clans"
   add_foreign_key "tanks", "nations"
+  add_foreign_key "tanks", "tiers"
   add_foreign_key "tanks", "vehicle_types"
 end
