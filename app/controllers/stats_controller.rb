08d43ca35cc028780e8187e865aa1b5e52224e3e
Marvin Hopp <accounts_aimdrol@hotmail.com>
2017-08-08 03:12:25 +0200

    Added methods to stats controller for the separate distribution views

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -2,4 +2,17 @@ class StatsController < ApplicationController
   def distribution
 
   end
+  
+  def distribution_marks
+
+  end
+  
+  def distribution_winrate
+
+  end
+  
+  def distribution_wn8
+
+  end
+  
 end
