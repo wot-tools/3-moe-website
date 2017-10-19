43907ddc82534f8f3fed0df37eb9a7defae39ebe
filip <bitbucket@xn--feld-4qa.de>
2017-10-19 12:34:11 +0200

    VehicleType as enum

--- mono_apps/WGApiClient/Tank.cs
+++ mono_apps/WGApiClient/Tank.cs
@@ -21,9 +21,47 @@ namespace WGApi
         public Nations Nation { get; set; }
         [JsonProperty("tier")]
         public int Tier { get; set; }
-        [JsonProperty("type")]
-        public string VehicleType { get; set; }
+        [JsonProperty("type"), JsonConverter(typeof(VehicleTypeConverter))]
+        public VehicleTypes VehicleType { get; set; }
         [JsonProperty("images")]
         public Icons Icons { get; set; }
+
+        private class VehicleTypeConverter : JsonConverter
+        {
+            private static VehicleTypeFactory Factory = new VehicleTypeFactory();
+
+            public override bool CanConvert(Type objectType)
+            {
+                return true;
+            }
+
+            public override bool CanWrite => false;
+
+            public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+            {
+                return Factory.GetVehicleType(serializer.Deserialize<string>(reader));
+            }
+
+            public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+            {
+                throw new NotImplementedException();
+            }
+
+            private class VehicleTypeFactory
+            {
+                public VehicleTypes GetVehicleType(string s)
+                {
+                    switch (s)
+                    {
+                        case "heavyTank": return VehicleTypes.Heavy;
+                        case "mediumTank": return VehicleTypes.Medium;
+                        case "lightTank": return VehicleTypes.Light;
+                        case "AT-SPG": return VehicleTypes.TD;
+                        case "SPG": return VehicleTypes.SPG;
+                        default: throw new NotImplementedException();
+                    }
+                }
+            }
+        }
     }
 }
---
+++ mono_apps/WGApiClient/VehicleTypes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WGApi
+{
+    public enum VehicleTypes
+    {
+        None = 0,
+        Heavy = 1 << 0,
+        Medium = 1 << 1,
+        Light = 1 << 2,
+        TD = 1 << 3,
+        SPG = 1 << 4,
+
+        All = ~(~0 << 5)
+    }
+}
